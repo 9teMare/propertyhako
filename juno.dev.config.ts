08b41d6d9bd0de3df2766b7b1943bd0f5ecfc7b5
9teMare <wang.zihao@u.nus.edu>
2024-05-09 14:58:03 +0800

    feat: init juno satellite

--- juno.dev.config.ts
+++ juno.dev.config.ts
@@ -1,26 +1,26 @@
-import {defineDevConfig} from '@junobuild/config';
+import { defineDevConfig } from "@junobuild/config";
 
 export default defineDevConfig(() => ({
-  satellite: {
-    collections: {
-      db: [
-        {
-          collection: 'notes',
-          read: 'managed' as const,
-          write: 'managed' as const,
-          memory: 'stable' as const,
-          mutablePermissions: true
-        }
-      ],
-      storage: [
-        {
-          collection: 'images',
-          read: 'managed' as const,
-          write: 'managed' as const,
-          memory: 'stable' as const,
-          mutablePermissions: true
-        }
-      ]
-    }
-  }
+    satellite: {
+        collections: {
+            db: [
+                {
+                    collection: "demo",
+                    read: "managed" as const,
+                    write: "managed" as const,
+                    memory: "stable" as const,
+                    mutablePermissions: true,
+                },
+            ],
+            storage: [
+                {
+                    collection: "images",
+                    read: "managed" as const,
+                    write: "managed" as const,
+                    memory: "stable" as const,
+                    mutablePermissions: true,
+                },
+            ],
+        },
+    },
 }));
