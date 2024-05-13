e0ea661d19498e2550947c7daeaf21eef78a91f7
9teMare <wang.zihao@u.nus.edu>
2024-05-13 21:17:08 +0800

    Create profile.ts

---
+++ src/types/profile.ts
@@ -0,0 +1,13 @@
+export interface TenantProfile {
+    age: number;
+    gender: "male" | "female";
+    occupation: string;
+    pass: string;
+    budget: number;
+    pax: number;
+    relationshipBetweenTenants?: string;
+    smoking: boolean;
+    pet: boolean;
+    startFrom: string;
+    duration: number;
+}
